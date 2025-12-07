ead347d623e11cc796e5b03e209d20396b0621d7
Imran Shaik <ski.imran89immu@gmail.com>
2025-12-07 17:32:12 +0530

    Added Firebase Config

--- static/js/firebase-config.js
+++ static/js/firebase-config.js
@@ -1,6 +1,18 @@
-// --- FIREBASE REMOVED FOR LOCAL VERSION ---
-// This file is kept to maintain directory structure but is no longer active.
-const auth = null;
-const db = null;
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 
-export { auth, db };
+const firebaseConfig = {
+    apiKey: "AIzaSyBF8EEc5eOaOP7J21ZFrtb7kOjpFdYI4yk",
+    authDomain: "lsb-96306.firebaseapp.com",
+    projectId: "lsb-96306",
+    storageBucket: "lsb-96306.firebasestorage.app",
+    messagingSenderId: "540966454802",
+    appId: "1:540966454802:web:a3e12460c6bda31a20bacb"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+// Export the auth service so other files can use it
+export { auth };
